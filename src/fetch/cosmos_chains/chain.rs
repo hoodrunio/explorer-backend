86ebe55ea5f6c6fe9f90aabbb257bba0cd54ae3f
Berzan <berzan@tutanota.com>
2022-11-03 20:37:44 +0300

    add: `Chain` trait

--- src/fetch/cosmos_chains/chain.rs
+++ src/fetch/cosmos_chains/chain.rs
@@ -3,3 +3,10 @@ pub struct ChainUrls {
     rest_api: &'static str,
 }
 
+/** The trait that provides common methods of chains. */
+pub trait Chain {
+    /** Returns the name of the chain. */
+    fn get_name() -> &'static str;
+    /** Returns the `ChainUrls` of the chain. */
+    fn get_urls() -> ChainUrls;
+}
