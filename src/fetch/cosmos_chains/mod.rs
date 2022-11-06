9a3e6e7ae4141cf23a74861378e1a07b362ef24b
Berzan <berzan@tutanota.com>
2022-11-06 06:10:13 +0300

    update: `mod.rs` file

--- src/fetch/cosmos_chains/mod.rs
+++ src/fetch/cosmos_chains/mod.rs
@@ -1,2 +1,16 @@
+mod axelar;
 mod common;
+mod cosmos;
 mod evmos;
+mod kyve;
+mod secret;
+mod celestia;
+mod osmosis;
+
+pub use axelar::Axelar;
+pub use cosmos::Cosmos;
+pub use evmos::Evmos;
+pub use kyve::Kyve;
+pub use secret::Secret;
+pub use celestia::Celestia;
+pub use osmosis::Osmosis;
