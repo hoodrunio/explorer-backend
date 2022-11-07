f1e7ffe4c51270ab65830345ff467fc4123c5910
Berzan <berzan@tutanota.com>
2022-11-07 20:25:43 +0300

    create: `types` module

--- src/fetch/common_chain/mod.rs
+++ src/fetch/common_chain/mod.rs
@@ -1,8 +1,16 @@
 mod blocks;
+mod chain;
 mod others;
 mod requests;
-mod chain;
 mod transactions;
 
-
 pub use chain::Chain;
+
+pub mod types {
+    use super::*;
+
+    pub use blocks::*;
+    pub use others::*;
+    pub use requests::*;
+    pub use transactions::*;
+}
