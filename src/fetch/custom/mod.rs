4ea6ad3eed1fdc4fc635a6a1ea43ce9e5a48ec71
Berzan <berzan@tutanota.com>
2022-11-07 13:57:40 +0300

    create: `mod.rs` file

---
+++ src/fetch/custom/mod.rs
@@ -0,0 +1,12 @@
+pub use super::chains::*;
+
+/*
+ * This module is for defining custom methods for chains.
+ *
+ * To define custom methods:
+ *    - Create a new file with the name of a chain.
+ *    - Define the custom methods inside the file.
+ *    - Don't forget to use `async-trait` macro.
+ * 
+ * 
+ */
