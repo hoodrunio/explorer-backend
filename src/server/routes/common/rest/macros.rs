91c219a68e34281baa1c48df578ec8cb3a58559c
Berzan <berzan@tutanota.com>
2022-11-09 11:28:12 +0300

    create: `macros.rs` file

---
+++ src/server/routes/common/rest/macros.rs
@@ -0,0 +1,28 @@
+// Defining AXELAR.
+
+use crate::chain;
+
+chain!({
+    ident: Axelar,
+    name: "axelar",
+    logo: "htpps://logo.com/svg",
+    prefix: "axelar",
+    sdk_ver: 45,
+    decimals: 6,
+    rpc_url: "www.rpc",
+    rest_url: "www.rest",
+});
+
+// Defining OSMOSIS.
+
+chain!({
+    ident: Osmosis,
+    name: "osmosis",
+    logo: "htpps://logo.com/svg",
+    prefix: "osmo",
+    sdk_ver: 45,
+    decimals: 6,
+    rpc_url: "www.rpc",
+    rest_url: "www.rest",
+});
+
