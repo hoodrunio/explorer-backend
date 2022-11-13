788f5d9f8595c298d4938b919ff1e627b0b99699
Berzan <berzan@tutanota.com>
2022-11-13 21:56:53 +0300

    create: `price_chart.rs` file

---
+++ src/data/price_chart.rs
@@ -0,0 +1,40 @@
+use std::collections::VecDeque;
+
+use serde::{Deserialize, Serialize};
+
+/// The chart for the price of the native coin.
+#[derive(Deserialize, Serialize, Debug)]
+pub struct PriceChart {
+    pub inner: VecDeque<ChartItem>,
+}
+
+impl PriceChart {
+    /// Creates a new price chart.
+    pub fn new() -> PriceChart {
+        PriceChart { inner: VecDeque::new() }
+    }
+
+    /// Adds a new price to the chart.
+    pub fn add_new(&mut self, price: f64) {
+        let t = chrono::offset::Utc::now().timestamp_millis() as u32;
+
+        match self.inner.get(self.inner.len() - 1) {
+            Some(last_item) => {
+                if t > last_item.t + 3_600_000 {
+                    self.inner.push_back(ChartItem { p: price, t });
+
+                    if self.inner.len() > 24 {
+                        self.inner.pop_front();
+                    };
+                }
+            }
+            _ => (),
+        };
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct ChartItem {
+    pub p: f64, // price
+    pub t: u32, // timestamp
+}
