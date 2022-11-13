9655b8a25999f9e977fac9268784afb53271cf55
Berzan <berzan@tutanota.com>
2022-11-13 22:01:41 +0300

    create: `delegations.rs` file

---
+++ src/routes/rest/delegations.rs
@@ -0,0 +1,51 @@
+use crate::{
+    fetch::rest::others::{PaginationConfig, Response},
+    state::State,
+};
+
+use actix_web::{
+    get,
+    web::{Data, Json, Path},
+    Responder,
+};
+
+// ======== Delegation Methods ========
+
+#[get("{chains}/delegations/{delegator_address}")]
+pub async fn delegations(path: Path<(String, String)>, chains: Data<State>) -> impl Responder {
+    let (chain, delegator_addr) = path.into_inner();
+
+    Json(match chains.get(&chain) {
+        Ok(chain) => chain
+            .get_delegations(&delegator_addr, PaginationConfig::new().limit(100).reverse())
+            .await
+            .into(),
+        Err(err) => Response::Error(err),
+    })
+}
+
+#[get("axelar/unbonding-delegations/{delegator_address}")]
+pub async fn unbonding_delegations(path: Path<(String, String)>, chains: Data<State>) -> impl Responder {
+    let (chain, delegator_addr) = path.into_inner();
+
+    Json(match chains.get(&chain) {
+        Ok(chain) => chain
+            .get_delegations_unbonding(&delegator_addr, PaginationConfig::new().limit(100).reverse())
+            .await
+            .into(),
+        Err(err) => Response::Error(err),
+    })
+}
+
+#[get("axelar/redelegations/{delegator_address}")]
+pub async fn redelegations(path: Path<(String, String)>, chains: Data<State>) -> impl Responder {
+    let (chain, delegator_addr) = path.into_inner();
+
+    Json(match chains.get(&chain) {
+        Ok(chain) => chain
+            .get_redelegations(&delegator_addr, PaginationConfig::new().limit(100).reverse())
+            .await
+            .into(),
+        Err(err) => Response::Error(err),
+    })
+}
