6997c388f3e799bd659d43d96390d95622003c20
Berzan <berzan@tutanota.com>
2022-11-13 21:55:05 +0300

    remove: `/server` folder

--- src/server/mod.rs
+++
@@ -1,6 +0,0 @@
-mod routes;
-mod state;
-mod web_server;
-mod web_socket;
-
-pub use web_server::start_web_server;
--- src/server/routes/mod.rs
+++
@@ -1,5 +0,0 @@
-mod common;
-mod custom;
-
-pub use common::*;
-pub use custom::*;
