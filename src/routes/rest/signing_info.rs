6268e91ce221f182d837a15923889c4b9281a53a
Berzan <berzan@tutanota.com>
2022-11-13 22:02:34 +0300

    create: `signing_info.rs` file

---
+++ src/routes/rest/signing_info.rs
@@ -0,0 +1,19 @@
+use crate::{fetch::rest::others::Response, state::State};
+
+use actix_web::{
+    get,
+    web::{Data, Json, Path},
+    Responder,
+};
+
+// ======== Signing Information Methods ========
+
+#[get("{chain}/signing/{cons_address}")]
+pub async fn signing(path: Path<(String, String)>, chains: Data<State>) -> impl Responder {
+    let (chain, cons_addr) = path.into_inner();
+
+    Json(match chains.get(&chain) {
+        Ok(chain) => chain.get_signing_info(&cons_addr).await.into(),
+        Err(err) => Response::Error(err),
+    })
+}
