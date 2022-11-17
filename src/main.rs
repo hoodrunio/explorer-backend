a128cc8f761e39b98d1f0302b32aa5835b40b421
Berzan <berzan@tutanota.com>
2022-11-17 03:56:17 +0300

    add: `macros` module`

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use crate::server::start_web_server;
 mod chain;
 mod data;
 mod fetch;
+mod macros;
 mod routes;
 mod server;
 mod state;
@@ -10,5 +11,6 @@ mod utils;
 #[tokio::main]
 async fn main() {
     println!("Server is running...");
-    start_web_server().await.unwrap()
+
+    start_web_server().await.unwrap();
 }
