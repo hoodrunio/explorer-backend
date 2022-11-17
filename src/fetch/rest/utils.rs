1bf1f919aa2ffe199232fda4e1d3678af7744df1
Berzan <berzan@tutanota.com>
2022-11-17 17:16:47 +0300

    add: new methods to `Chain` struct

--- src/fetch/rest/utils.rs
+++ src/fetch/rest/utils.rs
@@ -1,3 +1,5 @@
+use std::ops::Div;
+
 use super::others::{DenomAmount, Pagination, PaginationConfig};
 use crate::chain::Chain;
 
@@ -8,5 +10,21 @@ impl Chain {
 
     pub fn cons_addr(&self, addr: &str) -> String {
         format!("{}{}", self.inner.cons_prefix, addr)
+    }
+
+    pub fn calc_amount_u128_to_u64(&self, amount: u128) -> u64 {
+        (amount / (self.inner.decimals_pow * 100) as u128) as u64
+    }
+
+    pub fn calc_amount_u128_to_f64(&self, amount: u128) -> f64 {
+        (amount / (self.inner.decimals_pow) as u128) as f64 / 100.0
+    }
+
+    pub fn calc_amount_f64_to_u64(&self, amount: f64) -> u64 {
+        amount as u64 / (self.inner.decimals_pow * 100)
+    }
+
+    pub fn calc_amount_f64_to_f64(&self, amount: f64) -> f64 {
+        amount / (self.inner.decimals_pow as f64 * 100.0)
     }
 }
