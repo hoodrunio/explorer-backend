630b981a31329e68f9fbabb1d8cf09c334658a72
Berzan <berzan@tutanota.com>
2022-11-17 04:07:20 +0300

    update: logic based on the changes made for `Chain` struct

--- src/fetch/rest/utils.rs
+++ src/fetch/rest/utils.rs
@@ -3,10 +3,10 @@ use crate::chain::Chain;
 
 impl Chain {
     pub fn valoper_addr(&self, addr: &str) -> String {
-        format!("{}{}", self.valoper_prefix, addr)
+        format!("{}{}", self.inner.valoper_prefix, addr)
     }
 
     pub fn cons_addr(&self, addr: &str) -> String {
-        format!("{}{}", self.cons_prefix, addr)
+        format!("{}{}", self.inner.cons_prefix, addr)
     }
 }
