bea3adc274c61471b976141c4189b8b6eb035075
Berzan <berzan@tutanota.com>
2022-11-17 04:07:53 +0300

    update: logic based on the changes made for `Chain` struct

--- src/routes/rest/delegations.rs
+++ src/routes/rest/delegations.rs
@@ -5,47 +5,46 @@ use crate::{
 
 use actix_web::{
     get,
-    web::{Data, Json, Path},
+    web::{Data, Json, Path, Query},
     Responder,
 };
+
+use super::QueryParams;
 
 // ======== Delegation Methods ========
 
 #[get("{chain}/delegations/{delegator_address}")]
-pub async fn delegations(path: Path<(String, String)>, chains: Data<State>) -> impl Responder {
+pub async fn delegations(path: Path<(String, String)>, chains: Data<State>, query: Query<QueryParams>) -> impl Responder {
     let (chain, delegator_addr) = path.into_inner();
 
+    let config = PaginationConfig::new().limit(5).page(query.page.unwrap_or(1));
+
     Json(match chains.get(&chain) {
-        Ok(chain) => chain
-            .get_delegations(&delegator_addr, PaginationConfig::new().limit(100).reverse())
-            .await
-            .into(),
+        Ok(chain) => chain.get_delegations(&delegator_addr, config).await.into(),
         Err(err) => Response::Error(err),
     })
 }
 
 #[get("{chain}/unbonding-delegations/{delegator_address}")]
-pub async fn unbonding_delegations(path: Path<(String, String)>, chains: Data<State>) -> impl Responder {
+pub async fn unbonding_delegations(path: Path<(String, String)>, chains: Data<State>, query: Query<QueryParams>) -> impl Responder {
     let (chain, delegator_addr) = path.into_inner();
 
+    let config = PaginationConfig::new().limit(5).page(query.page.unwrap_or(1));
+
     Json(match chains.get(&chain) {
-        Ok(chain) => chain
-            .get_delegations_unbonding(&delegator_addr, PaginationConfig::new().limit(100).reverse())
-            .await
-            .into(),
+        Ok(chain) => chain.get_delegations_unbonding(&delegator_addr, config).await.into(),
         Err(err) => Response::Error(err),
     })
 }
 
 #[get("{chain}/redelegations/{delegator_address}")]
-pub async fn redelegations(path: Path<(String, String)>, chains: Data<State>) -> impl Responder {
+pub async fn redelegations(path: Path<(String, String)>, chains: Data<State>, query: Query<QueryParams>) -> impl Responder {
     let (chain, delegator_addr) = path.into_inner();
 
+    let config = PaginationConfig::new().limit(5).page(query.page.unwrap_or(1));
+
     Json(match chains.get(&chain) {
-        Ok(chain) => chain
-            .get_redelegations(&delegator_addr, PaginationConfig::new().limit(100).reverse())
-            .await
-            .into(),
+        Ok(chain) => chain.get_redelegations(&delegator_addr, config).await.into(),
         Err(err) => Response::Error(err),
     })
 }
