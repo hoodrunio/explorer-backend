447723c98cf297f34279ac0793d7ba6fbe4f3a21
Berzan <berzan@tutanota.com>
2022-11-17 04:06:58 +0300

    update: logic based on the changes made for `Chain` struct

--- src/fetch/rest/requests.rs
+++ src/fetch/rest/requests.rs
@@ -1,15 +1,14 @@
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 
-use super::others::{DenomAmount, Pagination, PaginationConfig};
 use crate::chain::Chain;
 
 impl Chain {
     /// Makes a request to the RPC node.
     pub(super) async fn rpc_request<T: DeserializeOwned>(&self, path: &str, query: &[(&'static str, String)]) -> Result<T, String> {
         // Create the URL request to.
-        let url = format!("{}{}", self.rpc_url, path);
+        let url = format!("{}{}", self.inner.rpc_url, path);
 
-        match self.client.get(url).query(query).send().await {
+        match self.inner.client.get(url).query(query).send().await {
             Ok(res) => match res.json::<RPCResponse<T>>().await {
                 Ok(res_json) => match res_json {
                     RPCResponse::Success(res) => Ok(res.result),
@@ -24,9 +23,9 @@ impl Chain {
     /// Makes a request to the REST API node.
     pub(super) async fn rest_api_request<T: DeserializeOwned>(&self, path: &str, query: &[(&'static str, String)]) -> Result<T, String> {
         // Create the URL request to.
-        let url = format!("{}{}", self.rest_url, path);
+        let url = format!("{}{}", self.inner.rest_url, path);
 
-        match self.client.get(url).query(query).send().await {
+        match self.inner.client.get(url).query(query).send().await {
             Ok(res) => {
                 if res.status().is_success() {
                     match res.json::<RestResponse<T>>().await {
