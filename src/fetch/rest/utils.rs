e3c6db81ab02e9fe1d00221d9a4c6b9ba4631b40
Berzan <berzan@tutanota.com>
2022-11-18 14:12:14 +0300

    add: new methods

--- src/fetch/rest/utils.rs
+++ src/fetch/rest/utils.rs
@@ -12,19 +12,27 @@ impl Chain {
         format!("{}{}", self.inner.cons_prefix, addr)
     }
 
+    pub fn base_to_valoper(&self, base_addr: &str) -> Result<String, String> {
+        if base_addr.starts_with(self.inner.base_prefix) {
+            Ok(format!("{}{}", self.inner.valoper_prefix, &base_addr[self.inner.base_prefix.len()..]))
+        } else {
+            Err(format!("Address is mistaken, '{}'.", base_addr))
+        }
+    }
+
     pub fn calc_amount_u128_to_u64(&self, amount: u128) -> u64 {
-        (amount / (self.inner.decimals_pow * 100) as u128) as u64
+        (amount / (self.inner.decimals_pow * 10000) as u128) as u64
     }
 
     pub fn calc_amount_u128_to_f64(&self, amount: u128) -> f64 {
-        (amount / (self.inner.decimals_pow) as u128) as f64 / 100.0
+        (amount / (self.inner.decimals_pow) as u128) as f64 / 10000.0
     }
 
     pub fn calc_amount_f64_to_u64(&self, amount: f64) -> u64 {
-        amount as u64 / (self.inner.decimals_pow * 100)
+        amount as u64 / (self.inner.decimals_pow * 10000)
     }
 
     pub fn calc_amount_f64_to_f64(&self, amount: f64) -> f64 {
-        amount / (self.inner.decimals_pow as f64 * 100.0)
+        amount / (self.inner.decimals_pow as f64 * 10000.0)
     }
 }
