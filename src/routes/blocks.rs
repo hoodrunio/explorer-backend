712ae7c6189b54ffe339589528bb060e7c9b5cb6
Berzan <berzan@tutanota.com>
2022-12-04 06:33:17 +0300

    add: `last_ten_blocks` route

--- src/routes/blocks.rs
+++ src/routes/blocks.rs
@@ -1,3 +1,4 @@
+use crate::routes::OutRestResponse;
 use crate::{fetch::others::Response, state::State};
 use actix_web::{
     get,
@@ -34,7 +35,7 @@ pub async fn headers_by_heights(path: Path<(String, String)>, chains: Data<State
     let (chain, min_and_max_height) = path.into_inner();
 
     Json(match chains.get(&chain) {
-        Ok(chain) => match min_and_max_height.split_once("-") {
+        Ok(chain) => match min_and_max_height.split_once('-') {
             Some((min_height, max_height)) => match (min_height.parse(), max_height.parse()) {
                 (Ok(min_height), Ok(max_height)) => Response::Success(chain.get_block_headers(min_height, max_height).await),
                 _ => Response::Error(format!("{} is mistaken!", min_and_max_height)),
@@ -53,6 +54,19 @@ pub async fn latest_headers(path: Path<String>, chains: Data<State>) -> impl Res
         Ok(chain) => match chain.get_block_headers_last_20().await {
             Ok(headers) => Response::Success(headers),
             Err(err) => Response::Error(err),
+        },
+        Err(err) => Response::Error(err),
+    })
+}
+
+#[get("{chain}/last-ten-blocks")]
+pub async fn last_ten_blocks(path: Path<String>, chains: Data<State>) -> impl Responder {
+    let chain = path.into_inner();
+
+    Json(match chains.get(&chain) {
+        Ok(chain) => match chain.inner.data.last_ten_blocks.queue.lock() {
+            Ok(last_ten_blocks) => Response::Success(OutRestResponse::new(Json(last_ten_blocks.clone()), 0)),
+            _ => Response::Error("An internal error occured.".to_string()),
         },
         Err(err) => Response::Error(err),
     })
