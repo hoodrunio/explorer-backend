3ab961d6bd2f865acad726a932b69587aaf5ab27
amorf <fatihermetin@gmail.com>
2023-01-20 00:51:48 +0300

    Add action prop to evm poll db model

--- src/database/evm_polls.rs
+++ src/database/evm_polls.rs
@@ -3,10 +3,11 @@ use crate::fetch::validators::{ValidatorListValidatorCommission, ValidatorStatus
 
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
 pub struct EvmPoll {
-    pub timestamp: String,
+    pub timestamp: u64,
     pub tx_height: u64,
     pub poll_id: String,
     pub status: String,
+    pub action: String,
     pub evm_tx_id: String,
     pub participants_operator_addresses: Vec<String>,
 }
