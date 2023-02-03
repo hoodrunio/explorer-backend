d070d2c19b617e3269cb774c70f7737c63439739
amorf <fatihermetin@gmail.com>
2023-02-04 01:46:10 +0300

    Add validator heartbeats router

--- src/routes/heartbeats.rs
+++ src/routes/heartbeats.rs
@@ -4,6 +4,7 @@ use actix_web::{
     web::{Data, Json, Path},
 };
 use actix_web::web::Query;
+use serde::{Deserialize, Serialize};
 
 use crate::{fetch::others::Response, state::State};
 use crate::chain::Chain;
@@ -12,7 +13,7 @@ use crate::routes::{extract_chain, QueryParams, TNRAppError, TNRAppSuccessRespon
 // ====== Heart Beats Methods ======
 
 #[get("{chain}/validator/heartbeats/{operator_address}")]
-pub async fn validator_hearbeats(path: Path<(String, String)>, chains: Data<State>) -> Result<impl Responder, TNRAppError> {
+pub async fn validator_hearbeats(path: Path<(String, String)>, chains: Data<State>, body: Json<ValidatorHeartbeatsQBody>) -> Result<impl Responder, TNRAppError> {
     let (chain, operator_address) = path.into_inner();
 
     let chain = extract_chain(&chain, chains)?;
@@ -21,6 +22,14 @@ pub async fn validator_hearbeats(path: Path<(String, String)>, chains: Data<Stat
         return Err(TNRAppError::from(String::from(format!("Evm polls not supported for {}", &chain.config.name))));
     };
 
-    let data: String = chain.get_val_heartbeats(&operator_address).await?;
+    let data = chain.get_val_heartbeats(operator_address, body.into_inner()).await?;
     Ok(TNRAppSuccessResponse::new(data))
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct ValidatorHeartbeatsQBody {
+    pub from_block: Option<u64>,
+    // pub size:u64,
+    pub sender: String,
+    pub to_block: Option<u64>,
 }
