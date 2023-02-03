c15765e91001d0e5ade544ebcf17ba7255fb3089
amorf <fatihermetin@gmail.com>
2023-02-04 02:04:46 +0300

    Add validator heartbeats route body validation

--- src/fetch/heartbeats.rs
+++ src/fetch/heartbeats.rs
@@ -6,7 +6,7 @@ use crate::database::HeartbeatForDb;
 use crate::routes::{TNRAppError, ValidatorHeartbeatsQBody};
 
 impl Chain {
-    pub async fn get_val_heartbeats(&self, operator_address: String, hearbeats_body: ValidatorHeartbeatsQBody) -> Result<Vec<HeartbeatForDb>, TNRAppError> {
+    pub async fn get_val_heartbeats(&self, operator_address: String, heartbeats_query: HeartbeatsQuery) -> Result<Vec<HeartbeatForDb>, TNRAppError> {
         let query = doc! {"operator_address": operator_address};
         let val_voter_address = match self.database.find_validator(query).await {
             Ok(res) => {
@@ -20,7 +20,7 @@ impl Chain {
 
         let match_pipe = doc! {"$match":{"sender": val_voter_address}};
         let mut pipeline = vec![match_pipe];
-        match (hearbeats_body.from_block, hearbeats_body.to_block) {
+        match (heartbeats_query.from_block, heartbeats_query.to_block) {
             (Some(from), (Some(to))) => {
                 let range_match_pipe = doc! {"$match":{"period_height":{"$gte":from as i64,"$lt":to as i64}}};
                 pipeline.push(range_match_pipe);
@@ -31,5 +31,25 @@ impl Chain {
         let heartbeats = self.database.find_heartbeats(pipeline).await?;
 
         Ok(heartbeats)
+    }
+}
+
+pub struct HeartbeatsQuery {
+    pub sender: String,
+    pub from_block: Option<i64>,
+    pub to_block: Option<i64>,
+}
+
+impl HeartbeatsQuery {
+    pub fn new(sender: String, from_block: Option<i64>, to_block: Option<i64>) -> Result<Self, String> {
+        if (from_block.is_some() && to_block.is_none()) || (to_block.is_some() && from_block.is_none()) {
+            return Err(String::from("Please specify from_block and to_block properties together"));
+        };
+
+        Ok(Self {
+            sender,
+            from_block,
+            to_block,
+        })
     }
 }
--- src/routes/heartbeats.rs
+++ src/routes/heartbeats.rs
@@ -8,6 +8,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::{fetch::others::Response, state::State};
 use crate::chain::Chain;
+use crate::fetch::heartbeats::HeartbeatsQuery;
 use crate::routes::{extract_chain, QueryParams, TNRAppError, TNRAppSuccessResponse};
 
 // ====== Heart Beats Methods ======
@@ -22,14 +23,20 @@ pub async fn validator_hearbeats(path: Path<(String, String)>, chains: Data<Stat
         return Err(TNRAppError::from(String::from(format!("Evm polls not supported for {}", &chain.config.name))));
     };
 
-    let data = chain.get_val_heartbeats(operator_address, body.into_inner()).await?;
+    let heartbeats_body = body.into_inner();
+    let heartbeats_query = HeartbeatsQuery::new(
+        heartbeats_body.sender.clone(),
+        heartbeats_body.from_block.clone(),
+        heartbeats_body.to_block.clone())?;
+
+    let data = chain.get_val_heartbeats(operator_address, heartbeats_query).await?;
     Ok(TNRAppSuccessResponse::new(data))
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct ValidatorHeartbeatsQBody {
-    pub from_block: Option<u64>,
+    pub from_block: Option<i64>,
     // pub size:u64,
     pub sender: String,
-    pub to_block: Option<u64>,
+    pub to_block: Option<i64>,
 }
