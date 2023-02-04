8f26a40be79379bc8889665543cfa7e9e88ee779
amorf <fatihermetin@gmail.com>
2023-02-05 00:00:54 +0300

    Create common db file

---
+++ src/database/common.rs
@@ -0,0 +1,15 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct ListDbResult<T> {
+    /// Array of validators.
+    pub validators: Vec<T>,
+    /// Pagination.
+    pub pagination: PaginationDb,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct PaginationDb {
+    pub page: u16,
+    pub total: u16,
+}
