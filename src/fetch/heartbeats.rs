0d2e432316f642e846b0df8d7c1772a9f822b625
amorf <fatihermetin@gmail.com>
2023-02-05 01:19:39 +0300

    Delete sender and pagination to hearbeats route

--- src/fetch/heartbeats.rs
+++ src/fetch/heartbeats.rs
@@ -2,11 +2,12 @@ use mongodb::bson::doc;
 use serde::{Deserialize, Serialize};
 
 use crate::chain::Chain;
-use crate::database::HeartbeatForDb;
-use crate::routes::{TNRAppError, ValidatorHeartbeatsQBody};
+use crate::database::{HeartbeatForDb, PaginationDb};
+use crate::fetch::others::PaginationConfig;
+use crate::routes::{HeartbeatsListResp, TNRAppError, ValidatorHeartbeatsQBody};
 
 impl Chain {
-    pub async fn get_val_heartbeats(&self, operator_address: String, heartbeats_query: HeartbeatsQuery) -> Result<Vec<HeartbeatForDb>, TNRAppError> {
+    pub async fn get_val_heartbeats(&self, operator_address: String, heartbeats_query: HeartbeatsQuery, config: PaginationConfig) -> Result<HeartbeatsListResp, TNRAppError> {
         let query = doc! {"operator_address": operator_address};
         let val_voter_address = match self.database.find_validator(query).await {
             Ok(res) => {
@@ -28,26 +29,38 @@ impl Chain {
             _ => {}
         };
 
-        let heartbeats = self.database.find_heartbeats(pipeline).await?;
+        let heartbeats = self.database.find_paginated_heartbeats(pipeline, config).await?;
+        let res = HeartbeatsListResp::from_db_list(heartbeats)?;
 
-        Ok(heartbeats)
+        Ok(res)
     }
 }
 
+#[derive(Deserialize, Serialize, Debug)]
 pub struct HeartbeatsQuery {
-    pub sender: String,
     pub from_block: Option<i64>,
     pub to_block: Option<i64>,
 }
 
+#[derive(Deserialize, Serialize, Debug)]
+pub struct HeartbeatsListElement {
+    pub tx_hash: String,
+    pub height: u64,
+    pub period_height: u64,
+    pub timestamp: u64,
+    pub signatures: Vec<String>,
+    pub sender: String,
+    pub key_ids: Vec<String>,
+    pub id: String,
+}
+
 impl HeartbeatsQuery {
-    pub fn new(sender: String, from_block: Option<i64>, to_block: Option<i64>) -> Result<Self, String> {
+    pub fn new(from_block: Option<i64>, to_block: Option<i64>) -> Result<Self, String> {
         if (from_block.is_some() && to_block.is_none()) || (to_block.is_some() && from_block.is_none()) {
             return Err(String::from("Please specify from_block and to_block properties together"));
         };
 
         Ok(Self {
-            sender,
             from_block,
             to_block,
         })
--- src/routes/heartbeats.rs
+++ src/routes/heartbeats.rs
@@ -8,13 +8,15 @@ use serde::{Deserialize, Serialize};
 
 use crate::{fetch::others::Response, state::State};
 use crate::chain::Chain;
-use crate::fetch::heartbeats::HeartbeatsQuery;
+use crate::database::{HeartbeatForDb, ListDbResult, PaginationDb};
+use crate::fetch::heartbeats::{HeartbeatsListElement, HeartbeatsQuery};
+use crate::fetch::others::PaginationConfig;
 use crate::routes::{extract_chain, QueryParams, TNRAppError, TNRAppSuccessResponse};
 
 // ====== Heart Beats Methods ======
 
 #[get("{chain}/validator/heartbeats/{operator_address}")]
-pub async fn validator_hearbeats(path: Path<(String, String)>, chains: Data<State>, body: Json<ValidatorHeartbeatsQBody>) -> Result<impl Responder, TNRAppError> {
+pub async fn validator_hearbeats(path: Path<(String, String)>, chains: Data<State>, body: Option<Json<ValidatorHeartbeatsQBody>>, query: Query<QueryParams>) -> Result<impl Responder, TNRAppError> {
     let (chain, operator_address) = path.into_inner();
 
     let chain = extract_chain(&chain, chains)?;
@@ -22,21 +24,55 @@ pub async fn validator_hearbeats(path: Path<(String, String)>, chains: Data<Stat
     if &chain.config.name != "axelar" {
         return Err(TNRAppError::from(String::from(format!("Evm polls not supported for {}", &chain.config.name))));
     };
+    let config = PaginationConfig::new().limit(query.limit.unwrap_or(250)).page(query.page.unwrap_or(1));
 
-    let heartbeats_body = body.into_inner();
+    let (from, to) = match body {
+        None => { (None, None) }
+        Some(res) => {
+            let body_inner = res.into_inner();
+            (body_inner.from_block, body_inner.to_block)
+        }
+    };
     let heartbeats_query = HeartbeatsQuery::new(
-        heartbeats_body.sender.clone(),
-        heartbeats_body.from_block.clone(),
-        heartbeats_body.to_block.clone())?;
+        from.clone(),
+        to.clone())?;
 
-    let data = chain.get_val_heartbeats(operator_address, heartbeats_query).await?;
+    let data = chain.get_val_heartbeats(operator_address, heartbeats_query, config).await?;
     Ok(TNRAppSuccessResponse::new(data))
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct ValidatorHeartbeatsQBody {
     pub from_block: Option<i64>,
-    // pub size:u64,
     pub sender: String,
     pub to_block: Option<i64>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct HeartbeatsListResp {
+    pub list: Vec<HeartbeatsListElement>,
+    pub pagination: PaginationDb,
+}
+
+
+impl HeartbeatsListResp {
+    pub fn from_db_list(list_db_result: ListDbResult<HeartbeatForDb>) -> Result<Self, TNRAppError> {
+        let heartbeats = list_db_result.list.into_iter().map(|heartbeat| {
+            HeartbeatsListElement {
+                tx_hash: heartbeat.tx_hash.clone(),
+                height: heartbeat.height.clone(),
+                period_height: heartbeat.period_height.clone(),
+                timestamp: heartbeat.timestamp.clone(),
+                signatures: heartbeat.signatures.clone(),
+                sender: heartbeat.sender.clone(),
+                key_ids: heartbeat.key_ids.clone(),
+                id: heartbeat.id.clone(),
+            }
+        }).collect();
+
+        Ok(Self {
+            list: heartbeats,
+            pagination: list_db_result.pagination,
+        })
+    }
 }
