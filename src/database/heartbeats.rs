a23e96e72adc6ec077336d5367d110dfb5820b75
amorf <fatihermetin@gmail.com>
2023-02-05 20:36:07 +0300

    Add heartbeat raw data struct for db

--- src/database/heartbeats.rs
+++ src/database/heartbeats.rs
@@ -1,7 +1,18 @@
 use serde::{Deserialize, Serialize};
 
+use crate::fetch::heartbeats::HeartbeatStatus;
+
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
 pub struct Heartbeat {
+    pub period_height: u64,
+    pub status: HeartbeatStatus,
+    pub sender: String,
+    pub id: String,
+    pub heartbeat_raw: Option<HeartbeatRaw>,
+}
+
+#[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
+pub struct HeartbeatRaw {
     pub tx_hash: String,
     pub height: u64,
     pub period_height: u64,
@@ -9,5 +20,5 @@ pub struct Heartbeat {
     pub signatures: Vec<String>,
     pub sender: String,
     pub key_ids: Vec<String>,
-    pub id: String,
 }
+
