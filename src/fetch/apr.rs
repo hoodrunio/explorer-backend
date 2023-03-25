7e74e335ed0488ad060ccc9ef011d8d098ce63fe
amorf <fatihermetin@gmail.com>
2023-03-25 15:25:25 +0300

    Refactor apr calculation

--- src/fetch/apr.rs
+++ src/fetch/apr.rs
@@ -1,4 +1,3 @@
-use reqwest::Method;
 use rust_decimal::prelude::{FromPrimitive, ToPrimitive};
 use serde::{Deserialize, Serialize};
 use tokio::join;
@@ -11,93 +10,44 @@ impl Chain {
     /// Reference: https://github.com/bro-n-bro/prometheus_exporter/blob/main/docs/APR%20calcucation.md#non-epoch-cosmos-based--blockchains
     pub async fn get_apr(&self) -> Result<f64, String> {
         const ANNUAL_PROVISION_MUL_RATIO: f64 = 365.3;
-
         // If the chain has epochs.
         if self.config.epoch {
+            let (epoch_prevision_res, staking_pool_res) = join!(self.get_epoch_provision(), self.get_staking_pool());
+            let epoch_provisions = epoch_prevision_res?;
+            let annual_provisions = epoch_provisions * ANNUAL_PROVISION_MUL_RATIO;
+            let bonded_tokens_amount = staking_pool_res?.value.bonded as f64;
+
             match self.config.name.as_str() {
                 "osmosis" => {
-                    let epoch_provisions = self.get_epoch_provision().await?;
-
-                    let osmosis_staking_pool_tokens_response = match self
-                        .external_rest_api_req::<OsmosisStakingPoolTokensResponse>(
-                            &self.client,
-                            Method::GET,
-                            "https://lcd.osmosis-1.bronbro.io/cosmos/staking/v1beta1/pool",
-                            &[],
-                        )
-                        .await
-                    {
-                        Ok(parsed_response) => parsed_response,
-                        Err(error) => return Err(error),
-                    };
-
-                    let bonded_tokens_amount = match osmosis_staking_pool_tokens_response.pool.bonded_tokens.parse::<f64>() {
-                        Ok(val) => val,
-                        Err(_) => return Err("FLOAT_PARSING_ERROR".to_string()),
-                    };
-
-                    let annual_provisions = epoch_provisions * ANNUAL_PROVISION_MUL_RATIO;
                     let staking_rewards_factor = 0.25;
-
+                    let bonded_tokens_amount = bonded_tokens_amount * (self.config.decimals_pow as f64 * 10000.0);
                     let apr = annual_provisions * staking_rewards_factor / bonded_tokens_amount;
 
                     Ok(apr)
                 }
                 "evmos" => {
-                    let evmos_decimal = self.config.decimals_pow as f64;
-                    let evmos_inflation_params_response = match self
-                        .external_rest_api_req::<EvmosInflationParamsResponse>(
-                            &self.client,
-                            Method::GET,
-                            "https://lcd.evmos-9001-2.bronbro.io/evmos/inflation/v1/params",
-                            &[],
-                        )
-                        .await
-                    {
-                        Ok(parsed_response) => parsed_response,
-                        Err(error) => return Err(error),
-                    };
+                    let evmos_decimal = self.config.decimals_pow as f64 * 10000.0;
 
-                    let staking_rewards_factor = match evmos_inflation_params_response
+                    let evmos_inflation_params_res = self
+                        .rest_api_request::<EvmosInflationParamsResponse>("/evmos/inflation/v1/params", &[])
+                        .await?;
+
+                    let staking_rewards_factor = evmos_inflation_params_res
                         .params
                         .inflation_distribution
                         .staking_rewards
                         .parse::<f64>()
-                    {
-                        Ok(value) => value,
-                        Err(_) => return Err("FLOAT_PARSING_ERROR".to_string()),
-                    };
+                        .map_err(|_| "FLOAT_PARSING_ERROR")?;
 
-                    let epoch_provisions = self.get_epoch_provision().await.map(|res| res / evmos_decimal)?;
+                    let epoch_provisions = epoch_provisions / evmos_decimal;
 
-                    let evmos_staking_pool_response = match self
-                        .external_rest_api_req::<EvmosStakingPoolResponse>(
-                            &self.client,
-                            Method::GET,
-                            "https://lcd.evmos-9001-2.bronbro.io/cosmos/staking/v1beta1/pool",
-                            &[],
-                        )
-                        .await
-                    {
-                        Ok(parsed_response) => parsed_response,
-                        Err(error) => return Err(error),
-                    };
-
-                    let bonded_tokens_amount = match evmos_staking_pool_response.pool.bonded_tokens.parse::<f64>() {
-                        Ok(value) => value / evmos_decimal,
-                        Err(_) => return Err("FLOAT_PARSING_ERROR".to_string()),
-                    };
                     let annual_provisions = epoch_provisions * ANNUAL_PROVISION_MUL_RATIO;
                     let apr = annual_provisions * staking_rewards_factor / bonded_tokens_amount;
 
                     Ok(apr)
                 }
                 "quicksilver" => {
-                    let (epoch_prevision_res, staking_pool_res, mint_params_res) =
-                        join!(self.get_epoch_provision(), self.get_staking_pool(), self.get_mint_params());
-                    let epoch_prevision_number = epoch_prevision_res?;
-                    let bonded_tokens = staking_pool_res?.value.bonded;
-                    let mint_params = mint_params_res?;
+                    let mint_params = self.get_mint_params().await?;
                     let staking_rewards_factor = mint_params
                         .params
                         .distribution_proportions
@@ -106,69 +56,41 @@ impl Chain {
                         .map_err(|_| "Failed to parse staking rewards factor".to_string())?;
 
                     let epoch_prevision = self
-                        .calc_tnr_decimal_amount(TnrDecimal::from_f64(epoch_prevision_number).unwrap_or_default(), None)
+                        .calc_tnr_decimal_amount(TnrDecimal::from_f64(epoch_provisions).unwrap_or_default(), None)
                         .to_f64()
                         .ok_or_else(|| "Failed to parse total supply".to_string())?;
 
-                    let annual_provision = epoch_prevision * 365.0;
-                    let apr = annual_provision * staking_rewards_factor / bonded_tokens as f64;
+                    let annual_provision = epoch_prevision * ANNUAL_PROVISION_MUL_RATIO;
+                    let apr = annual_provision * staking_rewards_factor / bonded_tokens_amount;
                     Ok(apr)
                 }
                 chain_name => Err(format!("APR for {chain_name} is not implemented.")),
             }
         } else {
+            let (params_all_res, staking_pool_res) = join!(self.get_params_all(), self.get_staking_pool());
+            let bonded_token_amount = staking_pool_res?.value.bonded as f64;
+            let community_tax = params_all_res?.value.distribution.community_tax;
             match self.config.name.as_str() {
                 "axelar" => {
-                    let chain_params = match self.get_params_all().await {
-                        Ok(res) => res.value,
-                        Err(error) => return Err(error),
-                    };
-
-                    let staking_pool = match self.get_staking_pool().await {
-                        Ok(res) => res.value,
-                        Err(error) => return Err(error),
-                    };
-
-                    //TODO Get total supply from remote
-                    let bonded_tokens_amount = staking_pool.bonded;
-                    let bonded_token_ratio = (bonded_tokens_amount as f64) / (1000000000.0);
-                    let inflation = match self.get_inflation_rate().await {
-                        Ok(res) => res,
-                        Err(error) => return Err(error),
-                    };
-                    let community_tax = chain_params.distribution.community_tax;
-
-                    Ok((inflation * (1.0 - community_tax)) / bonded_token_ratio)
+                    let inflation = self.get_inflation_rate().await?;
+                    let bonded_token_amount = bonded_token_amount / 1000000000.0;
+                    Ok((inflation * (1.0 - community_tax)) / bonded_token_amount)
                 }
                 _ => {
-                    let community_tax = match self.get_params_all().await {
-                        Ok(res) => res.value.distribution.community_tax,
-                        Err(error) => return Err(error),
-                    };
-                    let bonded_tokens_amount = match self.get_staking_pool().await {
-                        Ok(res) => (res.value.bonded * (self.config.decimals_pow * 10000)) as f64,
-                        Err(error) => return Err(error),
-                    };
+                    let bonded_token_amount = bonded_token_amount * (self.config.decimals_pow as f64 * 10000.0);
                     let annual_provisions = match self.get_annual_provisions().await {
                         Ok(res) => res.value,
                         Err(error) => return Err(error),
                     };
 
-                    //If needed call self.get_correction_annual_coefficient
-
                     let non_epoch_apr_calculator = NonEpochAprCalculator {
                         annual_provisions,
                         community_tax,
-                        bonded_tokens_amount,
+                        bonded_token_amount,
                         correction_annual_coefficient: None,
                     };
 
-                    let result = match non_epoch_apr_calculator.get_apr() {
-                        Ok(apr) => apr,
-                        Err(error) => return Err(error),
-                    };
-
-                    Ok(result)
+                    Ok(non_epoch_apr_calculator.get_apr())
                 } // chain_name => Err(format!("APR for {chain_name} is not implemented.")),
             }
         }
@@ -178,13 +100,13 @@ impl Chain {
 pub struct NonEpochAprCalculator {
     pub annual_provisions: f64,
     pub community_tax: f64,
-    pub bonded_tokens_amount: f64,
+    pub bonded_token_amount: f64,
     pub correction_annual_coefficient: Option<f64>,
 }
 
 impl NonEpochAprCalculator {
-    pub fn get_apr(&self) -> Result<f64, String> {
-        Ok((self.annual_provisions * (1.0 - self.community_tax) / self.bonded_tokens_amount) * self.correction_annual_coefficient.unwrap_or(1.0))
+    pub fn get_apr(&self) -> f64 {
+        (self.annual_provisions * (1.0 - self.community_tax) / self.bonded_token_amount) * self.correction_annual_coefficient.unwrap_or(1.0)
     }
 }
 
@@ -199,22 +121,6 @@ pub struct CosmosDistributionParams {
 #[derive(Deserialize, Serialize, Debug, PartialEq)]
 pub struct CosmosDistributionParamsResponse {
     pub params: CosmosDistributionParams,
-}
-
-#[derive(Deserialize, Serialize, Debug, PartialEq)]
-pub struct OsmosisStakingPoolTokensResponse {
-    pub pool: OsmosisStakingPoolTokens,
-}
-
-#[derive(Deserialize, Serialize, Debug, PartialEq)]
-pub struct OsmosisStakingPoolTokens {
-    pub not_bonded_tokens: String,
-    pub bonded_tokens: String,
-}
-
-#[derive(Deserialize, Serialize, Debug, PartialEq)]
-pub struct EpochProvisionResponse {
-    pub epoch_provisions: String,
 }
 
 #[derive(Deserialize, Serialize, Debug, PartialEq)]
@@ -244,26 +150,4 @@ pub struct EvmosInflationDistributionParam {
     staking_rewards: String,
     usage_incentives: String,
     community_pool: String,
-}
-
-#[derive(Deserialize, Serialize, Debug, PartialEq)]
-pub struct EvmosInflationEpochProvisionResponse {
-    pub epoch_mint_provision: EvmosInflationEpochProvision,
-}
-
-#[derive(Deserialize, Serialize, Debug, PartialEq)]
-pub struct EvmosInflationEpochProvision {
-    pub denom: String,
-    pub amount: String,
-}
-
-#[derive(Deserialize, Serialize, Debug, PartialEq)]
-pub struct EvmosStakingPoolResponse {
-    pub pool: EvmosStakingPool,
-}
-
-#[derive(Deserialize, Serialize, Debug, PartialEq)]
-pub struct EvmosStakingPool {
-    not_bonded_tokens: String,
-    bonded_tokens: String,
 }
