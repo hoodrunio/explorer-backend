cfd1ba8aebdd47cbe13c6292faf88f23e22d67b9
amorf <fatihermetin@gmail.com>
2023-03-29 22:09:09 +0300

    Add account vesting route

--- src/routes/account.rs
+++ src/routes/account.rs
@@ -17,3 +17,11 @@ pub async fn account(path: Path<(String, String)>, chains: Data<State>) -> Resul
     let data = chain.get_account_info(&account_address).await?;
     Ok(TNRAppSuccessResponse::new(data, None))
 }
+
+#[get("{chain}/vesting/{account_address}")]
+pub async fn account_vesting(path: Path<(String, String)>, chains: Data<State>) -> Result<impl Responder, TNRAppError> {
+    let (chain, account_address) = path.into_inner();
+    let chain = extract_chain(&chain, chains)?;
+    let data = chain.get_account_vesting_info(account_address).await?;
+    Ok(TNRAppSuccessResponse::new(data, None))
+}
