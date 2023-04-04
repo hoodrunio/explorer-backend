33e3767fe744212b453c3287bae8557bbb8d0e0c
amorf <fatihermetin@gmail.com>
2023-04-04 19:12:40 +0300

    Add tally result from traits for raw tally results

--- src/fetch/proposals.rs
+++ src/fetch/proposals.rs
@@ -11,8 +11,8 @@ use crate::{
         cosmos::{
             base::query::v1beta1::{PageRequest, PageResponse},
             distribution::v1beta1::CommunityPoolSpendProposal,
-            gov::v1::MsgExecLegacyContent,
-            gov::v1beta1::TextProposal,
+            gov::v1::{MsgExecLegacyContent, TallyResult as TallyResultV1},
+            gov::v1beta1::{TallyResult as TallyResultV1Beta1, TextProposal},
             params::v1beta1::ParameterChangeProposal,
             upgrade::v1beta1::SoftwareUpgradeProposal,
         },
@@ -992,6 +992,29 @@ pub struct RawProposalFinalTallyResult {
     pub raw_no: String,
     pub raw_no_with_veto: String,
 }
+
+impl From<TallyResultV1Beta1> for RawProposalFinalTallyResult {
+    fn from(tally_result: TallyResultV1Beta1) -> Self {
+        Self {
+            raw_yes: tally_result.yes,
+            raw_abstain: tally_result.abstain,
+            raw_no: tally_result.no,
+            raw_no_with_veto: tally_result.no_with_veto,
+        }
+    }
+}
+
+impl From<TallyResultV1> for RawProposalFinalTallyResult {
+    fn from(tally_result: TallyResultV1) -> Self {
+        Self {
+            raw_yes: tally_result.yes_count,
+            raw_abstain: tally_result.abstain_count,
+            raw_no: tally_result.no_count,
+            raw_no_with_veto: tally_result.no_with_veto_count,
+        }
+    }
+}
+
 #[derive(Serialize, Debug)]
 pub struct ProposalItem {
     /// Proposal ID.
