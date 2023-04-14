069c5f3382949efe86b757dec9cc808377bdc951
Tunahan Karlibas <dvt.tnhn.krlbs@icloud.com>
2023-04-14 10:50:59 +0300

    initial structure for new events

--- src/fetch/socket.rs
+++ src/fetch/socket.rs
@@ -1,3 +1,4 @@
+use std::collections::BTreeMap;
 use std::sync::Arc;
 
 use chrono::{DateTime, Utc};
@@ -72,7 +73,202 @@ const AXELAR_SUB_VOTE_TX: &str = r#"{
     }
 }"#;
 
+#[derive(Debug, Clone)]
+pub struct BaseTransaction {
+    /// `[ "F0E26D70191E27C8AB6249DE9C088B8C2812443CDF0DF04D7C83AE76A117C083" ]`
+    // #[serde(rename = "tx.hash")]
+    pub tx_hash: String,
+
+    /// `[ "2931697000000000aevmos" ]`
+    // #[serde(rename = "tx.fee")]
+    pub tx_fee: String,
+
+    /// `[ "8076531" ]`
+    // #[serde(rename = "tx.height")]
+    pub tx_height: String,
+
+    /// `[ "/ethermint.evm.v1.MsgEthereumTx" ]`
+    // #[serde(rename = "message.action")]
+    pub message_action: String,
+
+    /// `[ "1535902500000000aevmos" ]`
+    // #[serde(rename = "transfer.amount")]
+    pub transfer_amount: String,
+}
+
+impl BaseTransaction {
+    fn from_tx_events(ev: TXMap) -> Self {
+
+
+        Self {
+            tx_hash: ev["tx.hash"].get(0).unwrap().to_string(),
+            tx_fee: ev["tx.fee"].get(0).unwrap().to_string(),
+            tx_height: ev["tx.height"].get(0).unwrap().to_string(),
+            message_action: ev["message.action"].get(0).unwrap().to_string(),
+            transfer_amount: ev["transfer.amount"].get(0).unwrap().to_string(),
+
+        }
+    }
+}
+
+pub type TXMap = BTreeMap<String, Vec<String>>;
+
+#[derive(Debug, Clone)]
+pub struct ConfirmDepositStarted {
+    chain: String,
+    participants: String,
+    tx_id: String,
+    evm_deposit_address: String,
+    action: String,
+}
+
+impl ConfirmDepositStarted {
+    fn from_tx_events(ev: TXMap) -> Self {
+        Self {
+            chain: ev["axelar.evm.v1beta1.ConfirmDepositStarted.chain"].get(0).unwrap().to_string(),
+            participants: ev["axelar.evm.v1beta1.ConfirmDepositStarted.participants"].get(0).unwrap().to_string(),
+            tx_id: ev["axelar.evm.v1beta1.ConfirmDepositStarted.tx_id"].get(0).unwrap().to_string(),
+            evm_deposit_address: ev["axelar.evm.v1beta1.ConfirmDepositStarted.deposit_address"].get(0).unwrap().to_string(),
+            action: ev["message.action"].get(0).unwrap().to_string()
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ConfirmGatewayTxStartedEvents {
+    chain: String,
+    participants: PollParticipants,
+    tx_id: String,
+    message_action: String,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct PollParticipants {
+    poll_id: String,
+    participants: Vec<String>,
+}
+
+impl ConfirmGatewayTxStartedEvents {
+    fn from_tx_events(ev: TXMap) -> Self {
+        let participants = serde_json::from_str(ev["axelar.evm.v1beta1.ConfirmGatewayTxStarted.participants"].get(0).unwrap()).unwrap();
+        Self {
+            chain: ev["axelar.evm.v1beta1.ConfirmGatewayTxStarted.chain"].get(0).unwrap().to_string(),
+            participants,
+            tx_id: ev["axelar.evm.v1beta1.ConfirmGatewayTxStarted.tx_id"].get(0).unwrap().to_string(),
+            message_action: ev["message.action"].get(0).unwrap().to_string(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ConfirmKeyTransferStartedEvents {
+    chain: String,
+    participants: String,
+    tx_id: String,
+    message_action: String,
+}
+
+impl ConfirmKeyTransferStartedEvents {
+    fn from_tx_events(ev: TXMap) -> Self {
+        Self {
+            chain: ev["axelar.evm.v1beta1.ConfirmKeyTransferStarted.chain"].get(0).unwrap().to_string(),
+            participants: ev["axelar.evm.v1beta1.ConfirmKeyTransferStarted.participants"].get(0).unwrap().to_string(),
+            tx_id: ev["axelar.evm.v1beta1.ConfirmKeyTransferStarted.tx_id"].get(0).unwrap().to_string(),
+            message_action: ev["message.action"].get(0).unwrap().to_string(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct PollVoteEvent {
+    poll_state: String,
+}
+
+impl PollVoteEvent {
+    fn from_tx_events(ev: TXMap) -> Self {
+        Self {
+            poll_state: ev["axelar.vote.v1beta1.Voted.state"].get(0).unwrap().to_string()
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum ExtraEventData {
+    ConfirmDepositStarted(ConfirmDepositStarted),
+    ConfirmGatewayTxStarted(ConfirmGatewayTxStartedEvents),
+    ConfirmKeyTransferStarted(ConfirmKeyTransferStartedEvents),
+    PollVote(PollVoteEvent)
+}
+
+pub fn parse_transaction(events: TXMap) -> (BaseTransaction, Option<ExtraEventData>) {
+    let tx = BaseTransaction::from_tx_events(events.clone());
+
+    match tx.message_action.as_str() {
+        "ConfirmERC20Deposit" | "ConfirmDeposit" => {
+            if events.contains_key("axelar.evm.v1beta1.ConfirmDepositStarted.participants") {
+                let sp_tx = ConfirmDepositStarted::from_tx_events(events);
+                return (tx, Some(ExtraEventData::ConfirmDepositStarted(sp_tx)));
+                // dbg!(sp_tx);
+            }
+        },
+        "ConfirmGatewayTx" => {
+            if events.contains_key("axelar.evm.v1beta1.ConfirmGatewayTxStarted.participants") {
+                let sp_tx = ConfirmGatewayTxStartedEvents::from_tx_events(events);
+                return (tx, Some(ExtraEventData::ConfirmGatewayTxStarted(sp_tx)));
+                // dbg!(sp_tx);
+            }
+        },
+        "ConfirmTransferKey" => {
+            if events.contains_key("axelar.evm.v1beta1.ConfirmKeyTransferStarted.participants") {
+                let sp_tx = ConfirmKeyTransferStartedEvents::from_tx_events(events);
+                return (tx, Some(ExtraEventData::ConfirmKeyTransferStarted(sp_tx)));
+                // dbg!(sp_tx);
+            }
+        },
+        other => {
+            if events.contains_key("axelar.vote.v1beta1.Voted.state") {
+                // dbg!(other);
+                let sp_tx = PollVoteEvent::from_tx_events(events);
+                return (tx, Some(ExtraEventData::PollVote(sp_tx)));
+                // dbg!(sp_tx);
+            }
+        }
+
+        // m => { if m != "RefundMsgRequest" { dbg!(m); } }
+    }
+    (tx, None)
+}
+
+// #[derive(Debug, Clone)]
+// pub enum SpecialTransaction {
+// ProposalVoteTx { events: ProposalVoteEvents },
+// ConfirmERC20DepositStartedTx { events: ConfirmDepositStartedEvents },
+// ConfirmDepositStartedTx { events: ConfirmDepositStartedEvents },
+// ConfirmGatewayTxStartedTx { events: ConfirmGatewayTxStartedEvents },
+// ConfirmKeyTransferStartedTx { events: ConfirmKeyTransferStartedEvents },
+// VotedTx { events: VotedTxEvents },
+// }
+
 impl Chain {
+    async fn subscribe_events(&self, event_type: EventType, tx: Sender<(String, WsEvent)>) -> Result<(), String> {
+        let (client, driver) = WebSocketClient::new(self.config.wss_url.as_str())
+            .await
+            .map_err(|e| format!("Failed to connect to the websocket endpoint: {e}"))?;
+
+        let driver_handle = tokio::spawn(async move { driver.run().await });
+
+        let mut subs = client.subscribe(event_type.into())
+            .await
+            .map_err(|e| format!("Failed to subscribe to events: {e}"))?;
+
+        while let Some(res) = subs.next().await {
+            let Ok(ev) = res else {
+                continue;
+            };
+        }
+
+        Ok(())
+    }
     /// Subscribes to all the events.
     pub async fn subscribe_to_events(&self, tx: Sender<(String, WsEvent)>) -> Result<(), String> {
         // Define the URL.
