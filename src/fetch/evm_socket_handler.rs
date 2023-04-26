cb33c58dc5e0a809ced844f0b9bc537aa33ab587
amorf <fatihermetin@gmail.com>
2023-04-26 14:29:47 +0300

    Move heartbeat implementation to event handler

--- src/fetch/evm_socket_handler.rs
+++ src/fetch/evm_socket_handler.rs
@@ -184,31 +184,90 @@ impl EvmSocketHandler {
             }
         };
     }
-    pub async fn handle_evm_poll_any_message(&self, non_empty_message: SocketResultNonEmpty) {
-        let evm_poll_item = match non_empty_message.get_evm_poll_item(&self.chain).await {
-            Ok(res) => res,
-            Err(e) => {
-                tracing::error!("Could not get evm poll item {}", e);
-                return;
-            }
+    pub async fn heartbeat_handler(&self, chain: &Chain, current_height: u64, is_heartbeat_begin: bool, mut heartbeat_begin_height: u64) {
+        let heartbeat_block_check_range = 6;
+        if is_heartbeat_begin {
+            heartbeat_begin_height = current_height;
+
+            if let Ok(res) = chain
+                .database
+                .find_validators(Some(doc! {"$match":{"voter_address":{"$exists":true}}}))
+                .await
+            {
+                let period_height = heartbeat_begin_height + 1;
+                let mut initial_period_heartbeats = vec![];
+                for validator in res.into_iter() {
+                    match validator.voter_address.clone() {
+                        None => {}
+                        Some(sender_address) => {
+                            let generated_id = chain.generate_heartbeat_id(sender_address.clone(), period_height);
+                            let heartbeat = HeartbeatForDb {
+                                heartbeat_raw: None,
+                                period_height,
+                                status: HeartbeatStatus::Fail,
+                                sender: sender_address.clone(),
+                                id: generated_id,
+                            };
+                            initial_period_heartbeats.push(heartbeat);
+                        }
+                    };
+                }
+
+                match chain.database.add_heartbeat_many(initial_period_heartbeats).await {
+                    Ok(_) => {
+                        tracing::info!("Current period initial heartbeats inserted");
+                    }
+                    Err(_) => {
+                        tracing::info!("Current period initial heartbeats could not inserted");
+                    }
+                };
+            };
         };
 
-        let _ = evm_poll_item.upsert_participants(&self.chain.database).await;
+        if heartbeat_begin_height + heartbeat_block_check_range >= current_height {
+            let block_result = chain.get_block_result_by_height(Some(current_height)).await;
 
-        let evm_poll: EvmPollForDb = evm_poll_item.clone().into();
-        if let Err(e) = self
-            .ws_tx_sender
-            .send((self.chain.config.name.clone(), WsEvent::NewEvmPoll(evm_poll.clone())))
-        {
-            tracing::error!("Error dispatching evm poll event: {e}");
+            if let Ok(block_result) = block_result {
+                let mut block_res_txs_handler_futures = vec![];
+                for block_res_tx_res in block_result.value.txs_results {
+                    let sender_address = block_res_tx_res.get_sender_address().unwrap_or(String::from("")).clone();
+                    block_res_txs_handler_futures.push(async move {
+                        let heartbeat_info = chain.get_axelar_sender_heartbeat_info(&sender_address, current_height).await;
+                        if let Ok(info) = heartbeat_info {
+                            let period_height = heartbeat_begin_height + 1;
+                            let generated_id = chain.generate_heartbeat_id(info.sender.clone(), period_height);
+                            let sender = info.sender.clone();
+                            let heartbeat_raw = HeartbeatRawForDb {
+                                height: current_height,
+                                tx_hash: info.tx_hash.clone(),
+                                timestamp: info.timestamp as u64,
+                                signatures: info.signatures.clone(),
+                                key_ids: info.key_ids.clone(),
+                                sender: sender.clone(),
+                                period_height,
+                            };
+
+                            let db_heartbeat = HeartbeatForDb {
+                                id: generated_id.clone(),
+                                status: HeartbeatStatus::Success,
+                                heartbeat_raw: Some(heartbeat_raw),
+                                sender,
+                                period_height,
+                            };
+                            match chain.database.upsert_heartbeat(db_heartbeat).await {
+                                Ok(_) => {
+                                    tracing::info!("Successfully inserted heartbeat id {}", &generated_id)
+                                }
+                                Err(_) => {
+                                    tracing::error!("Could not inserted heartbeat id {}", &generated_id)
+                                }
+                            };
+                        };
+                    });
+                }
+
+                join_all(block_res_txs_handler_futures).await;
+            }
         }
-        match self.chain.database.upsert_evm_poll(evm_poll).await {
-            Ok(_) => {
-                tracing::info!("evm poll successfully created by poll id {}", &evm_poll_item.poll_id);
-            }
-            Err(e) => {
-                tracing::error!("evm poll could not created {}, Error: {}", &evm_poll_item.poll_id, e);
-            }
-        };
     }
 }
