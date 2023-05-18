d591c033c722d1d27c50b3ddfec6e72c3f784f4f
amorf <fatihermetin@gmail.com>
2023-05-18 22:39:51 +0300

    Add dashboard cron jobs

--- src/cron_jobs/params.rs
+++ src/cron_jobs/params.rs
@@ -1,6 +1,9 @@
+use tokio::join;
+
 use crate::chain::Chain;
 use crate::database::{
-    DistributionParamsForDb, GovParamsForDb, ParamsForDb, SlashingParamsForDb, StakingParamsForDb, TokenMarketPriceHistoriesForDb,
+    ChainDashboardInfoForDb, DistributionParamsForDb, GovParamsForDb, ParamsForDb, SlashingParamsForDb, StakingParamsForDb,
+    TokenMarketPriceHistoriesForDb,
 };
 
 impl Chain {
@@ -67,6 +70,61 @@ impl Chain {
                 tracing::error!("Error occured on inserting token prices to db {}", e);
             }
         };
+        Ok(())
+    }
+
+    pub async fn cron_job_chain_dashboard(&self) -> Result<(), String> {
+        let (inflation_rate, apr, staking_poll, total_supply, community_poll) = join!(
+            self.get_inflation_rate(),
+            self.get_apr(),
+            self.get_staking_pool(),
+            self.get_supply_by_denom(&self.config.main_denom),
+            self.get_community_pool(),
+        );
+
+        let mut def_inflation_rate = 0.0;
+        let mut def_apr = 0.0;
+        let mut def_total_unbonded = 0.0;
+        let mut def_total_bonded = 0.0;
+        let mut def_community_pool = 0;
+        let mut def_total_supply = "0".to_string();
+
+        let curren_dashboard_info = self.database.find_chain_dashboard_info().await;
+        if let Ok(cd) = curren_dashboard_info {
+            def_inflation_rate = cd.inflation_rate;
+            def_apr = cd.apr;
+            def_total_unbonded = cd.total_unbonded;
+            def_total_bonded = cd.total_bonded;
+            def_community_pool = cd.community_pool;
+            def_total_supply = cd.total_supply;
+        }
+
+        let inflation_rate = inflation_rate.unwrap_or(def_inflation_rate);
+        let apr = apr.unwrap_or(def_apr);
+
+        let mut total_unbonded = def_total_unbonded;
+        let mut total_bonded = def_total_bonded;
+        if let Ok(result) = staking_poll {
+            total_unbonded = result.value.unbonded as f64;
+            total_bonded = result.value.bonded as f64;
+        };
+
+        let total_supply = total_supply.map(|res| res.amount.to_string()).unwrap_or(def_total_supply);
+        let community_pool = community_poll.map(|res| res.value).unwrap_or(def_community_pool);
+
+        let chain_dashboard_info = ChainDashboardInfoForDb {
+            inflation_rate,
+            apr,
+            total_unbonded,
+            total_bonded,
+            total_supply,
+            community_pool,
+        };
+
+        if let Err(e) = self.database.upsert_chain_dashboard_info(chain_dashboard_info).await {
+            tracing::error!("Error occured on inserting chain dashboard info to db {}", e);
+        };
+
         Ok(())
     }
 }
